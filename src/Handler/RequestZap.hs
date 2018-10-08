c4fef2f0c4b52d759a48435b1ecfe5b60e27b3e9
SolviQorda <solvi.goard@gmail.com>
2018-10-08 10:17:12 +0100

    basics set up

---
+++ src/Handler/RequestZap.hs
@@ -0,0 +1,9 @@
+module Handler.RequestZap where
+
+import Import
+
+getRequestZapR :: Handler Html
+getRequestZapR = error "Not yet implemented: getRequestZapR"
+
+postRequestZapR :: Handler Html
+postRequestZapR = error "Not yet implemented: postRequestZapR"
---
+++ test/Handler/RequestZapSpec.hs
@@ -0,0 +1,14 @@
+module Handler.RequestZapSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getRequestZapR" $ do
+        error "Spec not implemented: getRequestZapR"
+
+
+    describe "postRequestZapR" $ do
+        error "Spec not implemented: postRequestZapR"
+
