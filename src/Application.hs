056a76402298b3cb9cdc86eb8381c81ac060ef22
SolviQorda <solvi.goard@gmail.com>
2018-11-06 16:47:11 +0000

    fixed 30 - added user dashboard, replacing profile

--- src/Application.hs
+++ src/Application.hs
@@ -57,6 +57,7 @@ import Handler.Profile
 import Handler.SetPaymentOptions
 import Handler.ViewAppts
 import Handler.UserDash
+import Handler.GenerateBookingUrl
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
