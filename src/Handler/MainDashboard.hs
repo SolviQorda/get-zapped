0c84d7c38a5da9081c58fe810eccd4e10a726330
SolviQorda <solvi.goard@gmail.com>
2018-11-20 13:05:34 +0000

    therapist dashboard displays warning

--- src/Handler/MainDashboard.hs
+++ src/Handler/MainDashboard.hs
@@ -4,11 +4,19 @@
 module Handler.MainDashboard where
 
 import Import
+import Yesod.Form.Bootstrap3
 
 getMainDashboardR :: UserId -> Handler Html
 getMainDashboardR userId = do
+  user <- runDB $ get404 userId
+  paymentSettings <- runDB $ selectList [TherapistPrefsTherapist ==. userId] [Desc TherapistPrefsTherapist]
   defaultLayout $ do
     $(widgetFile "/therapist/my-dash")
 
 postMainDashboardR :: UserId -> Handler Html
 postMainDashboardR userId = error "Not yet implemented: postMainDashboardR"
+
+paymentSettingsWarning :: [Entity TherapistPrefs] -> Text
+paymentSettingsWarning prefList
+  | Prelude.null prefList = pack "Warning: No Payment Settings Saved"
+  | otherwise     = pack " "
